6475a1ad9fc5905a43bd97cb6bc8c5bca9137497
darkgraycat <jburyanov@gmail.com>
2021-07-05 11:03:46 +0300

    rewrite index with actions

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = __importDefault(require("http"));
-var fs_1 = require("fs");
-var path_1 = __importDefault(require("path"));
+var actions_1 = require("./actions");
 var Methods;
 (function (Methods) {
     Methods["GET"] = "GET";
@@ -14,49 +13,18 @@ var Methods;
     Methods["DELETE"] = "DELETE";
 })(Methods || (Methods = {}));
 var server = http_1.default.createServer(function (req, res) {
-    var filePath = path_1.default.join(__dirname, 'public', req.url || '');
     switch (req.method) {
         case Methods.GET:
-            try {
-                fs_1.promises.readFile(filePath, 'utf-8')
-                    .then(function (data) { return res.end(data); });
-            }
-            catch (error) {
-                console.error(error.message);
-            }
+            actions_1.read(req).then(function (data) { return res.end(data); });
             break;
         case Methods.POST:
-            var data_1 = '';
-            req.on('data', function (chunk) { return data_1 += chunk; });
-            req.on('end', function () {
-                try {
-                    fs_1.promises.writeFile(filePath, data_1)
-                        .then(function () { return res.end('File succesfully changed'); });
-                }
-                catch (error) {
-                    console.error(error.message);
-                }
-            });
+            actions_1.write(req).then(function () { return res.end('File succesfully changed'); });
             break;
         case Methods.PATCH:
-            var body_1 = '';
-            req.on('data', function (chunk) { return body_1 += chunk; });
-            req.on('end', function () {
-                fs_1.promises.readFile(filePath, 'utf-8')
-                    .then(function (data) {
-                    var oldData = JSON.parse(data);
-                    var newData = JSON.parse(body_1);
-                    for (var key in newData) {
-                        oldData[key] = newData[key];
-                    }
-                    fs_1.promises.writeFile(filePath, JSON.stringify(oldData, null, 2))
-                        .then(function () { return res.end('File succesfully updated'); });
-                });
-            });
+            actions_1.append(req).then(function () { return res.end('File succesfully updated'); });
             break;
         case Methods.DELETE:
-            fs_1.promises.unlink(filePath)
-                .then(function () { return res.end('File succesfully deleted'); });
+            actions_1.remove(req).then(function () { return res.end('File succesfully removed'); });
             break;
         default:
             res.end();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import http, { Server } from 'http'
-import { promises as fsp } from 'fs'
-import path from 'path'
+import { read, write, append, remove } from './actions'
 
 enum Methods {
   GET = 'GET',
@@ -11,52 +10,22 @@ enum Methods {
 
 const server: Server = http.createServer((req, res) => {
 
-  const filePath: string = path.join(__dirname, 'public', req.url || '')
-
   switch (req.method) {
 
     case Methods.GET:
-      try {
-        fsp.readFile(filePath, 'utf-8')
-          .then(data => res.end(data))
-      } catch (error) {
-        console.error(error.message)
-      }
+      read(req).then(data => res.end(data))
       break
 
     case Methods.POST:
-      let data: string = ''
-      req.on('data', chunk => data += chunk)
-      req.on('end', () => {
-        try {
-          fsp.writeFile(filePath, data)
-            .then(() => res.end('File succesfully changed'))
-        } catch (error) {
-          console.error(error.message);
-        }
-      })
+      write(req).then(() => res.end('File succesfully changed'))
       break
 
     case Methods.PATCH:
-      let body: string = ''
-      req.on('data', chunk => body += chunk)
-      req.on('end', () => {
-        fsp.readFile(filePath, 'utf-8')
-          .then(data => {
-            const oldData = JSON.parse(data)
-            const newData = JSON.parse(body)
-            for (let key in newData) {
-              oldData[key] = newData[key]
-            }
-            fsp.writeFile(filePath, JSON.stringify(oldData, null, 2))
-              .then(() => res.end('File succesfully updated'))
-          })
-      })
+      append(req).then(() => res.end('File succesfully updated'))
       break
 
     case Methods.DELETE:
-      fsp.unlink(filePath)
-        .then(() => res.end('File succesfully deleted'))
+      remove(req).then(() => res.end('File succesfully removed'))
       break
 
     default:
